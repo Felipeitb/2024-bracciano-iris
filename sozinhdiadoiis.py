8a1602faa23e348b00085df1ed38dcaf3ba14a92
Your Name <you@example.com>
2024-11-28 16:10:31 +0100

    add petal plot

--- sozinhdiadoiis.py
+++ sozinhdiadoiis.py
@@ -10,8 +10,8 @@ X = iris.data
 y = iris.target
 
 # Visualizzazione della distribuzione delle classi
-plt.figure(figsize=(12, 4))
 
+plt.figure(figsize=(12, 4))
 # Plot per i sepali
 plt.subplot(1, 2, 1)
 for i in range(3):
@@ -22,9 +22,21 @@ plt.ylabel('Larghezza sepalo')
 plt.title('Distribuzione dei Sepali')
 plt.legend()
 
+
+
+# Plot per i sepali
+plt.subplot(1, 2, 2)
+for i in range(3):
+    mask = (y == i)
+    plt.scatter(X[mask, 2], X[mask,3], label=iris.target_names[i])
+plt.xlabel('Lunghezza petalo')
+plt.ylabel('Larghezza petalo')
+plt.title('Distribuzione dei petali')
+plt.legend()
+
+plt.show()
 # Plot per i petali
 
 # Ora tocca a te! Prova a fare il plot dei petali!
 # Poi fai commit e push
 
-plt.show()
